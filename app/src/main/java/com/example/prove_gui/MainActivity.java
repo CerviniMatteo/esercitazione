bdacf57707d5d343826ac2775eeac9d8af15f86d
CerviniMatteo <m.cervini1@campus.unimib.it>
2024-11-23 11:24:41 +0100

    Switch di computer

---
+++ app/src/main/java/com/example/prove_gui/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.prove_gui;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.prove_gui.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.example.prove_gui.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_map, R.id.navigation_saved, R.id.navigation_rating, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/example/prove_gui/ui/profile/ProfileViewModel.java
@@ -0,0 +1,11 @@
+package com.example.prove_gui.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+
+    public ProfileViewModel() {
+    }
+}
